9e01f8b5edc71784c1087bd219c4831ab5fa4182
milan-backend <milancharan847@gmail.com>
2026-03-17 20:04:55 +0530

    add

--- routes/auth/security.py
+++ routes/auth/security.py
@@ -7,8 +7,8 @@ import os
 load_dotenv()
 
 
-SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
